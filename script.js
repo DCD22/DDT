4274cdf8229d837c965e51d8717a042973c9b380
DCD22 <dan@MacBookPro.local>
2022-05-21 12:11:03 +0100

    Adding new pages

---
+++ script.js
@@ -0,0 +1,14 @@
+const faq = document.getElementsByClassName("faqQuestion");
+let i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let answer = this.nextElementSibling;
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    });
+}
