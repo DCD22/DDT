a66243f5d072061040a941eee0e40798d0c0639a
DCD22 <dan@MacBookPro.local>
2022-05-22 11:36:32 +0100

    Added JS functionality and more media queries

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ for (i = 0; i < faq.length; i++) {
 }
 
 // End of FAQ Accordion //
+
+// Hamburger Menu //
+
+const menu = document.querySelector("#navigationLinks");
+const menuItems = document.querySelectorAll(".navItem");
+const hamburger = document.querySelector(".hamburger");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+    } else {
+        menu.classList.add("showMenu");
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+// End of Hamburger Menu //
